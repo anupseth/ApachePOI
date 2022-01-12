53944660458f987763a8b028b226ad0ea66e61d0
anupseth <anupseth11@yahoo.com>
2022-01-12 12:35:17 +0530

    Create worksheet, row and set cell value

--- ApachePOI/src/main/java/com/poi/ApachePOI/ApachePoiApplication.java
+++ ApachePOI/src/main/java/com/poi/ApachePOI/ApachePoiApplication.java
@@ -4,6 +4,8 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.boot.CommandLineRunner;
@@ -28,6 +30,18 @@ public class ApachePoiApplication implements CommandLineRunner {
 	        }catch(Exception e) {  
 	            System.out.println(e.getMessage());  
 	        }  
+	        
+	        // Create row and set cell value
+			wb = new HSSFWorkbook();
+			try (OutputStream os = new FileOutputStream("CellCreation.xls")) {
+				Sheet sheet = wb.createSheet("New Sheet");
+				Row row = sheet.createRow(0);
+				Cell cell = row.createCell(0);
+				cell.setCellValue("Javatpoint");
+				wb.write(os);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			} 
 
 	}
 
